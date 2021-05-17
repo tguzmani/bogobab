616ba655352cdcaac1811bb0ea16857d3bf04ebb
Tomas <tomasg92@gmail.com>
2021-05-17 19:38:43 -0400

    Commit Inicial

---
+++ script.js
@@ -0,0 +1,37 @@
+const hero = document.querySelector('.hero')
+
+hero.style.backgroundImage = 'url(img/animals.jpg)'
+
+const imageFileNames = [
+  'animals',
+  'baby',
+  'couple',
+  'family',
+  'food',
+  'friends',
+  'vacation-beach',
+  'vacation-cold',
+  'wedding',
+].sort(() => 0.5 - Math.random())
+
+function changeHeroBackgroundImage(fileName) {
+  hero.style.backgroundImage = `url(img/${fileName}.jpg)`
+}
+
+function sliderAnimation(index) {
+  if (index === imageFileNames.length) index = 0
+
+  changeHeroBackgroundImage(imageFileNames[index])
+  setTimeout(() => sliderAnimation(index + 1), 5000)
+}
+
+function preloadImage(fileName) {
+  var img = new Image()
+  img.src = `url(img/${fileName}.jpg)`
+}
+
+function preloadImages(url) {
+  imageFileNames.forEach(fileName => preloadImage(fileName))
+}
+
+sliderAnimation(0)
